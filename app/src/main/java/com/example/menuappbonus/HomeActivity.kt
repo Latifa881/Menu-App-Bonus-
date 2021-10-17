14229db3c391efe85d8cb9cfad4d5393700d9c4f
Latifah AlMulhim <L-n-a-m@hotmail.com>
2021-10-17 16:28:59 +0300

    first commit

---
+++ app/src/main/java/com/example/menuappbonus/HomeActivity.kt
@@ -0,0 +1,38 @@
+package com.example.menuappbonus
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.Help -> {
+                val intent= Intent(this,HelpActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.Home -> {
+                val intent= Intent(this,HomeActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.About -> {
+                val intent= Intent(this,AboutActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
